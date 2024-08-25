a213d81263e23e508b439f62ea435c0c416f05b2
Davi Alemar <162141754+DaviAlemar@users.noreply.github.com>
2024-08-25 20:18:38 -0300

    .
    .

--- Projeto-1-APC.py
+++ Projeto-1-APC.py
@@ -1,20 +1,16 @@
-def frases(texto1,texto2):
- for c in range(2):
-  indini1 = texto1.find(f"{c}") + 2
-  indini2 = texto2.find(f"{c}") + 2
+def frases(texto1, texto2):
+    for c in range(72):
+        indini1 = texto1.find(f"{c}") + 2
+        indini2 = texto2.find(f"{c}") + 2
+        indfim1 = texto1[indini1:].find(f"{c}") + indini1 - 1
+        indfim2 = texto2[indini2:].find(f"{c}") + indini2 - 1
+        frase1 = texto1[indini1:indfim1]
+        frase2 = texto2[indini2:indfim2]
+        print(frase1)
+        print(frase2)
+        print(" ")
 
+textoportugues = input("Digite o texto em português: ")
+textoticuna = input("Digite o texto em ticuna: ")
 
-  indfim1 = texto1[indini1:].find(f"{c}")+ indini1 - 1
-  indfim2 = texto2[indini2:].find(f"{c}")+ indini2 -1
- 
-
-  frase1 = texto1[indini1:indfim1]
-  frase2 = texto2[indini2:indfim2]
-  print(frase1)
-  print(frase2)
-  print(" ")
-
-textoportugues = input()
-textoticuna = input()
-
-frases(textoportugues,textoticuna)
+frases(textoportugues, textoticuna)
